df36663ec907fb5815f1d226171ee9a068deb381
yutou <>
2019-09-06 17:58:31 +0800

    feat: hello world

---
+++ src/AliyunIoTSDK.cpp
@@ -0,0 +1,257 @@
+
+#include "AliyunIoTSDK.h"
+#include <PubSubClient.h>
+#include <SHA256.h>
+
+#define CHECK_INTERVAL 5000
+
+static const char *deviceName = NULL;
+static const char *productKey = NULL;
+static const char *deviceSecret = NULL;
+static const char *region = NULL;
+
+#define MQTT_PORT 1883
+
+#define SHA256HMAC_SIZE 32
+#define DATA_CALLBACK_SIZE 20
+
+#define ALINK_BODY_FORMAT "{\"id\":\"123\",\"version\":\"1.0\",\"method\":\"thing.event.property.post\",\"params\":%s}"
+#define ALINK_EVENT_BODY_FORMAT "{\"id\": \"123\",\"version\": \"1.0\",\"params\": %s,\"method\": \"thing.event.%s.post\"}"
+
+static unsigned long lastMs = 0;
+
+static PubSubClient *client = NULL;
+
+char AliyunIoTSDK::clientId[256] = "";
+char AliyunIoTSDK::mqttUsername[100] = "";
+char AliyunIoTSDK::mqttPwd[256] = "";
+
+char AliyunIoTSDK::ALINK_TOPIC_PROP_POST[150] = "";
+char AliyunIoTSDK::ALINK_TOPIC_PROP_SET[150] = "";
+char AliyunIoTSDK::ALINK_TOPIC_EVENT[150] = "";
+
+static String hmac256(const String &signcontent, const String &ds)
+{
+    byte hashCode[SHA256HMAC_SIZE];
+    SHA256 sha256;
+
+    const char *key = ds.c_str();
+    size_t keySize = ds.length();
+
+    sha256.resetHMAC(key, keySize);
+    sha256.update((const byte *)signcontent.c_str(), signcontent.length());
+    sha256.finalizeHMAC(key, keySize, hashCode, sizeof(hashCode));
+
+    String sign = "";
+    for (byte i = 0; i < SHA256HMAC_SIZE; ++i)
+    {
+        sign += "0123456789ABCDEF"[hashCode[i] >> 4];
+        sign += "0123456789ABCDEF"[hashCode[i] & 0xf];
+    }
+
+    return sign;
+}
+
+static void parmPass(JsonVariant parm)
+{
+    //    const char *method = parm["method"];
+    for (int i = 0; i < DATA_CALLBACK_SIZE; i++)
+    {
+        if (poniter_array[i].key)
+        {
+            bool hasKey = parm["params"].containsKey(poniter_array[i].key);
+            if (hasKey)
+            {
+                poniter_array[i].fp(parm["params"]);
+            }
+        }
+    }
+}
+// 所有云服务的回调都会首先进入这里，例如属性下发
+static void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    payload[length] = '\0';
+    Serial.println((char *)payload);
+
+    if (strstr(topic, AliyunIoTSDK::ALINK_TOPIC_PROP_SET))
+    {
+
+        StaticJsonDocument<200> doc;
+        DeserializationError error = deserializeJson(doc, payload); //反序列化JSON数据
+
+        if (!error) //检查反序列化是否成功
+        {
+            parmPass(doc.as<JsonVariant>()); //将参数传递后打印输出
+        }
+    }
+}
+
+// void AliyunIoTSDK::bind(MQTT_CALLBACK_SIGNATURE)
+// {
+
+//     //    return *this;
+// }
+static bool mqttConnecting = false;
+void AliyunIoTSDK::mqttCheckConnect()
+{
+    if (client != NULL && !mqttConnecting)
+    {
+        if (!client->connected())
+        {
+            Serial.println("Connecting to MQTT Server ...");
+            mqttConnecting = true;
+            if (client->connect(clientId, mqttUsername, mqttPwd))
+            {
+                Serial.println("MQTT Connected!");
+            }
+            else
+            {
+                Serial.print("MQTT Connect err:");
+                Serial.println(client->state());
+                // delay(65000);
+            }
+            mqttConnecting = false;
+        }
+    }
+}
+
+void AliyunIoTSDK::begin(WiFiClient &espClient,
+                         const char *_productKey,
+                         const char *_deviceName,
+                         const char *_deviceSecret,
+                         const char *_region)
+{
+
+    client = new PubSubClient(espClient);
+    productKey = _productKey;
+    deviceName = _deviceName;
+    deviceSecret = _deviceSecret;
+    region = _region;
+    long times = millis();
+    String timestamp = String(times);
+
+    sprintf(clientId, "%s|securemode=3,signmethod=hmacsha256,timestamp=%s|", deviceName, timestamp.c_str());
+
+    String signcontent = "clientId";
+    signcontent += deviceName;
+    signcontent += "deviceName";
+    signcontent += deviceName;
+    signcontent += "productKey";
+    signcontent += productKey;
+    signcontent += "timestamp";
+    signcontent += timestamp;
+
+    Serial.println("signcontent");
+    Serial.println(signcontent);
+    String pwd = hmac256(signcontent, deviceSecret);
+
+    strcpy(mqttPwd, pwd.c_str());
+
+    sprintf(mqttUsername, "%s&%s", deviceName, productKey);
+    sprintf(ALINK_TOPIC_PROP_POST, "/sys/%s/%s/thing/event/property/post", productKey, deviceName);
+    sprintf(ALINK_TOPIC_PROP_SET, "/sys/%s/%s/thing/service/property/set", productKey, deviceName);
+    sprintf(ALINK_TOPIC_EVENT, "/sys/%s/%s/thing/event", productKey, deviceName);
+
+    char domain[150];
+    sprintf(domain, "%s.iot-as-mqtt.%s.aliyuncs.com", productKey, region);
+    client->setServer(domain, MQTT_PORT); /* 连接WiFi之后，连接MQTT服务器 */
+    client->setCallback(callback);
+
+    mqttCheckConnect();
+}
+
+void AliyunIoTSDK::loop()
+{
+    if (millis() - lastMs >= CHECK_INTERVAL)
+    {
+        lastMs = millis();
+        mqttCheckConnect();
+    }
+    client->loop();
+}
+
+void AliyunIoTSDK::sendEvent(const char *eventId, const char *param)
+{
+    char topicKey[156];
+    sprintf(topicKey, "%s/%s/post", ALINK_TOPIC_EVENT, eventId);
+    char jsonBuf[1024];
+    sprintf(jsonBuf, ALINK_EVENT_BODY_FORMAT, param, eventId);
+    Serial.println(jsonBuf);
+    boolean d = client->publish(topicKey, jsonBuf);
+    Serial.print("publish:0 失败;1成功");
+    Serial.println(d);
+}
+void AliyunIoTSDK::sendEvent(const char *eventId)
+{
+    sendEvent(eventId, "{}");
+}
+
+void AliyunIoTSDK::send(const char *param)
+{
+
+    char jsonBuf[1024];
+
+    sprintf(jsonBuf, ALINK_BODY_FORMAT, param);
+    Serial.println(jsonBuf);
+    boolean d = client->publish(ALINK_TOPIC_PROP_POST, jsonBuf);
+    Serial.print("publish:0 失败;1成功");
+    Serial.println(d);
+}
+void AliyunIoTSDK::send(char *key, float number)
+{
+    char param[128];
+    sprintf(param, "{\"%s\":%f}", key, number);
+    send(param);
+}
+void AliyunIoTSDK::send(char *key, int number)
+{
+    char param[128];
+    sprintf(param, "{\"%s\":%d}", key, number);
+    send(param);
+}
+void AliyunIoTSDK::send(char *key, double number)
+{
+    char param[128];
+    sprintf(param, "{\"%s\":%f}", key, number);
+    send(param);
+}
+
+void AliyunIoTSDK::send(char *key, char *text)
+{
+    char param[1024];
+    sprintf(param, "{\"%s\":\"%s\"}", key, text);
+    send(param);
+}
+
+int AliyunIoTSDK::bindData(char *key, poniter_fun fp)
+{
+    int i;
+    for (i = 0; i < DATA_CALLBACK_SIZE; i++)
+    {
+        if (!poniter_array[i].fp)
+        {
+            poniter_array[i].key = key;
+            poniter_array[i].fp = fp;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int AliyunIoTSDK::unbindData(char *key)
+{
+    int i;
+    for (i = 0; i < DATA_CALLBACK_SIZE; i++)
+    {
+        if (!strcmp(poniter_array[i].key, key))
+        {
+            poniter_array[i].key = NULL;
+            poniter_array[i].fp = NULL;
+            return 0;
+        }
+    }
+    return -1;
+}
---
+++ src/AliyunIoTSDK.h
@@ -0,0 +1,113 @@
+#ifndef ALIYUN_IOT_SDK_H
+#define ALIYUN_IOT_SDK_H
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+
+typedef void (*poniter_fun)(JsonVariant ele); //定义一个函数指针
+
+typedef struct poniter_desc
+{
+  char *key;
+  poniter_fun fp;
+} poniter_desc, *p_poniter_desc;
+
+// 最多绑定20个回调
+static poniter_desc poniter_array[20];
+static p_poniter_desc p_poniter_array;
+
+class AliyunIoTSDK
+{
+private:
+  // mqtt 链接信息，动态生成的
+  static char mqttPwd[256];
+  static char clientId[256];
+  static char mqttUsername[100];
+
+  // 定时检查 mqtt 链接
+  static void mqttCheckConnect();
+
+public:
+  // 标记一些 topic 模板
+  static char ALINK_TOPIC_PROP_POST[150];
+  static char ALINK_TOPIC_PROP_SET[150];
+  static char ALINK_TOPIC_EVENT[150];
+
+  // 在主程序 loop 中调用，检查连接和定时发送信息
+  static void loop();
+
+  /**
+   * 初始化程序
+   * @param ssid wifi名
+   * @param passphrase wifi密码
+   */
+  static void begin(WiFiClient &espClient,
+                    const char *_productKey,
+                    const char *_deviceName,
+                    const char *_deviceSecret,
+                    const char *_region);
+
+  /**
+   * 发送数据
+   * @param param 字符串形式的json 数据，例如 {"${key}":"${value}"}
+   */
+  static void send(const char *param);
+  /**
+   * 发送 float 格式数据
+   * @param key 数据的 key
+   * @param number 数据的值
+   */
+  static void send(char *key, float number);
+  /**
+   * 发送 int 格式数据
+   * @param key 数据的 key
+   * @param number 数据的值
+   */
+  static void send(char *key, int number);
+  /**
+   * 发送 double 格式数据
+   * @param key 数据的 key
+   * @param number 数据的值
+   */
+  static void send(char *key, double number);
+  /**
+   * 发送 string 格式数据
+   * @param key 数据的 key
+   * @param text 数据的值
+   */
+  static void send(char *key, char *text);
+
+  /**
+   * 发送事件到云平台（附带数据）
+   * @param eventId 事件名，在阿里云物模型中定义好的
+   * @param param 字符串形式的json 数据，例如 {"${key}":"${value}"}
+   */
+  static void sendEvent(const char *eventId, const char *param);
+  /**
+   * 发送事件到云平台（空数据）
+   * @param eventId 事件名，在阿里云物模型中定义好的
+   */
+  static void sendEvent(const char *eventId);
+
+  /**
+   * 绑定回调，所有云服务下发的数据都会进回调
+   */
+  // static void bind(MQTT_CALLBACK_SIGNATURE);
+
+  /**
+   * 绑定事件回调，云服务下发的特定事件会进入回调
+   * @param eventId 事件名
+   */
+  // static void bindEvent(const char * eventId, MQTT_CALLBACK_SIGNATURE);
+  /**
+   * 绑定属性回调，云服务下发的数据包含此 key 会进入回调，用于监听特定数据的下发
+   * @param key 物模型的key
+   */
+  static int bindData(char *key, poniter_fun fp);
+  /**
+   * 卸载某个 key 的所有回调（慎用）
+   * @param key 物模型的key
+   */
+  static int unbindData(char *key);
+};
+#endif
